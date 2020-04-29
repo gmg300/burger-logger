151bd0d32dd8fae828178eca73f7f5a42b9dfb7d
Garrett <garrettgassensmith@gmail.com>
2020-04-29 14:40:18 -0500

    created front-end event listening and handling

--- public/assets/js/index.js
+++ public/assets/js/index.js
@@ -1,46 +1,30 @@
-// Make sure we wait to attach our handlers until the DOM is fully loaded.
-$(function() {
-    $(".change-sleep").on("click", function(event) {
-      var id = $(this).data("id");
-      var newSleep = $(this).data("newsleep");
-  
-      var newSleepState = {
-        sleepy: newSleep
-      };
-  
-      // Send the PUT request.
-      $.ajax("/api/cats/" + id, {
-        type: "PUT",
-        data: newSleepState
-      }).then(
-        function() {
-          console.log("changed sleep to", newSleep);
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
-    });
-  
-    $(".create-form").on("submit", function(event) {
-      // Make sure to preventDefault on a submit event.
-      event.preventDefault();
-  
-      var newCat = {
-        name: $("#ca").val().trim(),
-        sleepy: $("[name=sleepy]:checked").val().trim()
-      };
-  
-      // Send the POST request.
-      $.ajax("/api/cats", {
-        type: "POST",
-        data: newCat
-      }).then(
-        function() {
-          console.log("created new cat");
-          // Reload the page to get the updated list
-          location.reload();
-        }
-      );
+$(function () {
+  // CREATE - add new burger
+  $(".create-form").on("submit", function (e) {
+    e.preventDefault();
+    let newBurger = {
+      burger_name: $("#burger").val().trim(),
+    };
+    // Create burger
+    $.ajax("/api/burgers", {
+      type: "POST",
+      data: newBurger,
+    }).then(function () {
+      console.log("Burger created!");
+      location.reload();
     });
   });
-  
+  // UPDATE - Devour burger button functionality
+  $(".devour").on("click", function (e) {
+    let name = $(this).text();
+    let id = $(this).data("id");
+    // Update burger
+    $.ajax("/api/burgers/" + id, {
+      type: "PUT",
+      data: { devoured: true },
+    }).then(function () {
+      console.log("Burger DEVOURED!");
+      location.reload();
+    });
+  });
+});
